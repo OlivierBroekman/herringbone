67788305090e1b5b25310360ce7e557d72e31c1a
jiworo <pangilon@protonmail.com>
2025-02-06 18:51:03 +0100

    Add property usage

--- actions.py
+++ actions.py
@@ -4,24 +4,30 @@ import json
 class Action:
     def __init__(self, config: dict):
         self.__id = config["id"]
-        self.type = config["type"]
-        self.direction = config["direction"]
-        self.cost = config["cost"]
+        self._type = config["type"]
+        self._direction = config["direction"]
+        self._cost = config["cost"]
 
-    def set_type(self, new_type: str):
-        self.type = new_type
-    
-    def set_direction(self, new_direction: list[int]):
-        self.direction = new_direction
-    
-    def set_cost(self, new_cost: int):
-        self.cost = new_cost
+    @property
+    def type(self) -> str:
+        return self._type
 
-    def get_type(self) -> str:
-        return self.type
-    
-    def get_direction(self) -> list[int]:
+    @type.setter
+    def type(self, new_type: str):
+        self._type = new_type
+
+    @property
+    def direction(self) -> list[int]:
         return self.direction
-    
-    def get_cost(self) -> int:
+
+    @direction.setter
+    def direction(self, new_direction: list[int]):
+        self._direction = new_direction
+
+    @property
+    def cost(self) -> int:
         return self.cost
+
+    @cost.setter
+    def cost(self, new_cost: int):
+        self._cost = new_cost
