1b6ef8ffabaad0228b4d4e0068fe9717a8ab7366
Olivier Broekman <olivier.broekman@ru.nl>
2025-02-07 16:16:05 +0100

    Add cell.py setters and getters

--- src/env_core/episode/state_space/cell.py
+++ src/env_core/episode/state_space/cell.py
@@ -2,5 +2,22 @@ class Cell:
     """This class represents a cell in a gridworld"""
     # TODO: Change piece in constructor to instance of piece class, added str as placeholder
     def __init__(self, coordinates: list[int], piece: str = None):
-        self.coordinates = coordinates
-        self.piece = piece
+        self._coordinates = coordinates
+        self._piece = piece
+    
+    # Setters and getters
+    @property
+    def coordinates(self) -> list[int]:
+        return self.coordinates
+    
+    @coordinates.setter
+    def coordinates(self, new_coordinates: list[int]):
+        self.coordinates = new_coordinates
+    
+    @property
+    def piece(self) -> str:
+        return self.piece
+    
+    @piece.setter
+    def piece(self, new_piece):
+        self.piece = new_piece
