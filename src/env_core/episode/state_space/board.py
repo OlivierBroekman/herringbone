4f13014aa79eabc099ed02aea590b67935766477
jiworo <pangilon@protonmail.com>
2025-02-13 22:41:01 +0100

    Mod render map: dynamic cell length & color enum

--- src/env_core/episode/state_space/board.py
+++ src/env_core/episode/state_space/board.py
@@ -1,5 +1,17 @@
+from enum import Enum
 from pathlib import Path
 from src.env_core.utils.map_loader import load_map
+
+
+class Color(Enum):
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    BLUE = "\033[34m"
+    RESET = "\033[0m"
+
+    @staticmethod
+    def parse_color(color: str):
+        return Color.__members__.get(color.upper(), Color.RESET)
 
 
 class Board:
@@ -7,18 +19,21 @@ class Board:
         self.pieces = load_map(path_config, path_map)
 
     def observe_pieces(self) -> dict:
-        pass  # TODO ignore static pieces?
+        pass
 
-    def __str__(self):  # TODO Color enum
-        num_sep = len(self.pieces[0]) - 1
-        grid = f"╔{'═══╦' * num_sep}═══╗\n"
+    def __str__(self):
+        len_char = max(len(p.character or '') for row in self.pieces for p in row)
+        num_cols = len(self.pieces[0]) if self.pieces else 0
+
+        grid = f"╔{('═' * (len_char + 2) + '╦') * (num_cols - 1)}{'═' * (len_char + 1)}═╗\n"
 
         for i_row, row in enumerate(self.pieces):
-            grid += "║ " + " ║ ".join(
-                f"{p.color.encode('utf-8').decode('unicode_escape')}{(p.character or '?')}\033[0m" for p in
-                row) + " ║\n"
-            if i_row < len(self.pieces) - 1:
-                grid += f"╠{'═══╬' * num_sep}═══╣\n"
+            grid += f"║ {" ║ ".join(
+                f"{Color.parse_color(p.color).value}{((p.character or '?').center(len_char))}{Color.RESET.value}"
+                for p in row)} ║\n"
 
-        grid += f"╚{'═══╩' * num_sep}═══╝"
+            if i_row < len(self.pieces) - 1:
+                grid += "╠" + ('═' * (len_char + 2) + '╬') * (num_cols - 1) + '═' * (len_char + 1) + "═╣\n"
+
+        grid += f"╚{('═' * (len_char + 2) + '╩') * (num_cols - 1) + '═' * (len_char + 1)}═╝"
         return grid
