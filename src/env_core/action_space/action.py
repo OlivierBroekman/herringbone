e83bf75bb54acc866769bd89959042c7be474831
Olivier Broekman <olivier.broekman@ru.nl>
2025-02-21 09:53:06 +0100

    New getters & setters

--- src/env_core/action_space/action.py
+++ src/env_core/action_space/action.py
@@ -7,26 +7,20 @@ class Action:
         self._cost = config["cost"]
 
     # Setters and getters
-    @property
-    def type(self) -> str:
-        return self._type
-
-    @type.setter
-    def type(self, new_type: str):
+    def set_type(self, new_type: str): 
         self._type = new_type
-
-    @property
-    def direction(self) -> list[int]:
-        return self.direction
-
-    @direction.setter
-    def direction(self, new_direction: list[int]):
+    
+    def get_type(self) -> str:
+        return self._type
+    
+    def set_direction(self, new_direction: list[int]):
         self._direction = new_direction
 
-    @property
-    def cost(self) -> int:
-        return self.cost
-
-    @cost.setter
-    def cost(self, new_cost: int):
+    def get_direction(self) -> list[int]:
+        return self._direction
+    
+    def set_cost(self, new_cost: int):
         self._cost = new_cost
+    
+    def get_cost(self) -> int:
+        return self._cost
