74d7c4e5112491946cb71c32c465391a21fa33d0
Olivier Broekman <olivier.broekman@ru.nl>
2025-02-21 10:00:42 +0100

    New getters & setters

--- src/env_core/state_space/piece.py
+++ src/env_core/state_space/piece.py
@@ -1,3 +1,7 @@
+from xxlimited import new
+from traitlets import Bool
+
+
 class Piece:
     def __init__(
             self,
@@ -23,58 +27,50 @@ class Piece:
         return self._character
 
     # Setters and getters
-    @property
-    def is_terminal(self) -> bool:
+    def set_is_terminal(self, new_terminal: bool):
+        self._is_terminal = new_terminal
+    
+    def get_is_terminal(self) -> bool: 
         return self._is_terminal
 
-    @is_terminal.setter
-    def is_terminal(self, new_value: bool):
-        self._is_terminal = new_value
+    def set_location(self, new_loc: list[int]):
+        self._location = new_loc
 
-    @property
-    def location(self) -> list[int]:
+    def get_location(self) -> list[int]:
         return self._location
 
-    @location.setter
-    def location(self, new_location: list[int]):
-        self._location = new_location
-
-    @property
-    def start_location(self) -> list[int]:
+    def set_start_location(self, new_start_loc: list[int]):
+        self._start_location = new_start_loc
+    
+    def get_start_location(self) -> list[int]:
         return self._start_location
-
-    @property
-    def reward(self) -> float:
-        return self._reward
-
-    @reward.setter
-    def reward(self, new_reward: float):
+    
+    def set_reward(self, new_reward: float):
         self._reward = new_reward
+    
+    def get_reward(self) -> float:
+        return self._reward
+    
+    def set_is_visitable(self, new_visitable: bool):
+        self._is_visitable = new_visitable
 
-    @property
-    def is_visitable(self) -> bool:
+    def get_is_visitable(self) -> bool:
         return self._is_visitable
-
-    @is_visitable.setter
-    def is_visitable(self, new_value: bool):
-        self._is_visitable = new_value
-
-    @property
-    def character(self) -> str:
+    
+    def set_character(self, new_char: str):
+        self._character = new_char
+    
+    def get_character(self) -> str:
         return self._character
 
-    @character.setter
-    def character(self, new_char: str):
-        self._character = new_char
-
-    @property
-    def color(self) -> str:
-        return self._color
-
-    @property
-    def value(self) -> float:
-        return self._value
+    def set_color(self, new_color: str):
+        self._color = new_color
     
-    @value.setter
-    def value(self, new_value):
-        self._value = new_value
+    def get_color(self) -> str:
+        return self._color
+    
+    def set_value(self, new_val: float):
+        self._value = new_val
+
+    def get_value(self) -> float:
+        return self._value
