041fd32f558d07f53b6cea9ca879039cf36b075d
Olivier Broekman <olivier.broekman@ru.nl>
2025-02-24 10:56:40 +0100

    Finish transition matrix class, to be tested

--- src/env_core/transition_space/transition_matrix.py
+++ src/env_core/transition_space/transition_matrix.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import numpy as np
 
 from env_core.action_space.action import Action
 from env_core.state_space.board import Board
@@ -22,10 +23,24 @@ class TransitionMatrix:
                 action: Action
                 ) -> dict[Piece, dict[Piece, float]]:
             
-            states = [piece for row in board for piece in row]
-            direction = action.get_direction() # !
+            states = [piece for row in board.pieces for piece in row]
+
+            board_shape = (len(board.pieces), len(board.pieces[0]))
+
+            directions = action.get_directions()
+            probabilities = action.get_probabilities()
+
             inner = {piece: 0. for piece in states}
             outer = {piece: deepcopy(inner) for piece in states}
+
+            # 2D ASSUMPTION 
+            for piece in states:
+                idx = piece.get_location()
+                for direction, probability in zip(directions, probabilities):
+                    new_idx = [np.clip(idx[0] + direction[0], a_min=0, a_max=board_shape[0] - 1),
+                                np.clip(idx[1] + direction[1], a_min=0, a_max=board_shape[1] - 1)]
+                    new_piece = board.pieces[new_idx[0]][new_idx[1]]
+                    outer[piece][new_piece] += probability
             return outer
 
 
