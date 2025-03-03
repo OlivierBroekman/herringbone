a66f078e791f0bc6713a8a3d4630fe6abe3de3dd
Olivier Broekman <olivier.broekman@ru.nl>
2025-03-03 15:15:14 +0100

    Add policy iteration algorithm (not tested yet)

--- herringbone/env_core/algorithms/dynamic_programming/policyiteration.py
+++ herringbone/env_core/algorithms/dynamic_programming/policyiteration.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from herringbone import MDP, Algorithm, Policy, Board, Piece, Action
 
 class PolicyIteration(Algorithm):
@@ -96,12 +98,10 @@ class PolicyIteration(Algorithm):
             self
             ) -> Policy:
         
-        # Policy Evaluation
-        board = self._board
-
-        states = self._mdp.get_states()
-
-        # state_values = {state: 0 for state in states}
+        """
+        Policy Iteration algorithm based on pseudocode by: 
+        Reinforcement Learning: An Introduction by Sutton, R. & Barto, A.
+        """
 
         def expected_utility(
                 state: Piece,
@@ -124,14 +124,15 @@ class PolicyIteration(Algorithm):
         def policy_evaluation(
                 policy: Policy,
                 mdp: MDP
-                ):
+                ) -> dict[Piece, float]:
             """
             Policy Evaluation algorithm, based on 
             Reinforcement Learning: An Introduction by Sutton, R. & Barto, A.
             """
             
-            # We only need to initialise delta, as all states have already been given a default value of 0
+            # We only need to initialise delt and state values
             delta = 0
+            state_values = {state: 0 for state in states}
 
             while delta <= self.get_theta_threshold():
                 for state in states:
@@ -149,15 +150,63 @@ class PolicyIteration(Algorithm):
                                              * new_state.get_value()))
                     
                     state.set_value(new_value)
+                    state_values[state] = new_value
                     
                     # Stopping criterion
                     delta = max(delta, abs(old_value - state.value))
 
-                    # We do not need to return anything as the values are a field of the Piece class
+                    return state_values
         
-        # Policy Improvement
-        policy_stable = True
+        def action_evaluation(
+                state: Piece, 
+                state_values: dict[Piece, float]
+                ) -> dict[Action, float]:
+            """Action evaluation"""
 
-        for state in states:
-            old_action = self.policy
-    
+            actions = mdp.get_actions()
+
+            # Set initial action values to zero
+            action_values = {action: 0 for action in actions}
+
+            # Add the value of each action to the dict
+            for action in actions:
+                for new_state, transition_probability in mdp.get_transition_matrices()[action][state].items():
+                    action_values[action] += (transition_probability 
+                                              * (state.get_reward() 
+                                                 + self.get_gamma() 
+                                                 * state_values[new_state]))
+            return action_values
+        
+        states = self.get_mdp().get_states()
+        policy = self.get_policy()
+        mdp = self.get_mdp()
+
+        # Policy Improvement
+        while True:
+
+            policy_stable = True
+            state_values = policy_evaluation(policy=policy, mdp=mdp)
+
+            for state in states:
+                # Get current chosen action, based on the policy
+                keys = list(policy[state].keys())
+                chosen_action_idx = np.argmax(policy[state].values())
+                chosen_action = keys[chosen_action_idx]
+
+                # Get the value of each action
+                action_values = action_evaluation(state=state, state_values=state_values)
+
+                # Use np.argmax to determine the best action, based on the values
+                keys = list(action_values.keys())
+                best_action_idx = np.argmax(action_values.values())
+                best_action = keys[best_action_idx]
+
+                if chosen_action != best_action:
+                    policy_stable = False
+                
+                # Update the policy
+                policy[state] = {k: (1 if v == best_action else 0) for k, v in policy[state].items()}
+
+                # If we have converged, stop the algorithm and return the policy with its evaluation
+                if policy_stable:
+                    return policy, state_values
