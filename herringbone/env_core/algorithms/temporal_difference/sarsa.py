bd88bf3c0254bbb9ab11525db2d015e93059dc3e
jiworo <pangilon@protonmail.com>
2025-03-11 00:06:08 +0100

    Mod Sarsa

--- herringbone/env_core/algorithms/temporal_difference/sarsa.py
+++ herringbone/env_core/algorithms/temporal_difference/sarsa.py
@@ -1,47 +1,37 @@
+from typing import override
+
 from herringbone.env_core.action_space import Action
-from herringbone.env_core.algorithms.common.epsilon_greedy_policy import EpsilonGreedyPolicy
-from herringbone.env_core.mdp import MDP
+from herringbone.env_core.algorithms.temporal_difference.td_control import TDControl
 from herringbone.env_core.state_space.piece import Piece
 
 
-class Sarsa:
-    def __init__(self,
-        num_episodes: int, mdp: MDP,
-        alpha: float = .5, epsilon: float = .1, gamma: float = .9
-    ):
-        self.num_episodes = num_episodes  # TODO Bram episode class?
-        self.mdp = mdp
+class Sarsa(TDControl):
+    @override
+    def update_q_values(
+        self,
+        state: Piece, action: Action,
+        reward: float,
+        state_prime: Piece, action_prime: Action,
+    ) -> None:
+        """Sarsa update rule."""
+        self.q_values[state][action] += self.alpha * (
+            reward + self.gamma * self.q_values[state_prime][action_prime] - self.q_values[state][action]
+        )
 
-        self.alpha = alpha
-        self.epsilon = epsilon
-        self.gamma = gamma
-
-        self.q_values = self.init_q_values()
-        self.policy = EpsilonGreedyPolicy(self.mdp, epsilon, self.q_values)
-
-    def init_q_values(
-        self
-    ) -> dict[Piece, dict[Action, float]]:
-        """Initialize all Q-values to zero."""
-        return {state:
-                    {action: .0 for action in self.mdp.get_actions()} for state in self.mdp.get_states()}
-
-    def run(
-        self
-    ) -> dict[Piece, dict[Action, float]]:
+    @override
+    def run(self) -> dict[Piece, dict[Action, float]]:
         """Run Sarsa (on-policy-TD) to estimate Q-values."""
         for _ in range(self.num_episodes):
-            state = self.mdp.get_board().pieces[0][0]  # TODO hardcoded start state
+            state = self.mdp.get_board().pieces[0][0]  # TODO hardcoded
             action = self.policy.select_action(state, self.q_values)
 
             while not state.get_terminal():
-                state_prime = self.mdp.get_board().get_state_from_action(state, action)
+                state_prime = self.mdp.get_board().get_state_from_action(state, action)  # TODO via transition matrix
                 reward = state_prime.get_reward()
                 action_prime = self.policy.select_action(state_prime, self.q_values)
 
-                self.q_values[state][action] += self.alpha * (
-                        reward + self.gamma * self.q_values[state_prime][action_prime] - self.q_values[state][action]
-                )
+                self.update_q_values(state, action, reward, state_prime, action_prime)
+
                 state, action = state_prime, action_prime
 
         return self.q_values
