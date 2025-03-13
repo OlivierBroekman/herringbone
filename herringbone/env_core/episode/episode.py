02ecb914a8535cf84a31c48b874aedfa7cd720e7
Bram Bolt <bram.bolt@icloud.com>
2025-03-13 18:06:39 +0100

    start episode class

--- herringbone/env_core/episode/episode.py
+++ herringbone/env_core/episode/episode.py
@@ -1,5 +1,5 @@
 from typing import List
-from herringbone.env_core.state_space import Board
+from herringbone.env_core.state_space import Board, Piece
 from herringbone.env_core.algorithms.common import Policy
 from herringbone.env_core.mdp import MDP
 from dataclasses import dataclass
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Trajectory:
-    states: List[int]
+    states: List[Piece]
     actions: List[int]
     rewards: List[float]
 
@@ -19,24 +19,54 @@ class Episode:
         mdp: MDP,
         seed: int = 42,
         max_depth=1000,
-        start_state=[0, 0],
+        start_agent_coordinates=[0, 0],
+        live_render: bool= False
     ):
-        self.state = start_state
-        self.reward = None
+        # Problem Space
+        self.policy = policy
+        self.mdp = mdp
+        self.live_render = live_render
+        
+        # Settings
+        self.max_depth = max_depth
+        self.seed = seed
+     
+        # Innitalisation
+        self.agent_coordinates = start_agent_coordinates # another name? agent_coords does not really solve the length issue.
         self.trajectory: Trajectory = ([], [], [])
-        self.history: List[Board]
-        self.max_depth = 10000
 
     def run(self):
         """Runs an episode"""
-        for _ in range(self.max_depth):
-            self.step()
+        
+        depth = 0
+        state = self.mdp.get_board().pieces[self.agent_coordinates[0]][self.agent_coordinates[1]]
+        reward = None
+        while not state.get_terminal() and t < self.max_depth:   
+            if self.live_render:
+                pass
+               # utils.render(board, coords)
+            
+            # Select action
+            action = self.policy.select_action(state, self.q_values)
+            action = self.policy.select_action(state, self.policy.get_policy())
+            
+            # Update trajectory
+            self.trajectory.states.append(state)
+            self.trajectory.actions.append(action)
+            self.trajectory.actions.append(reward)
+            
+            # Get new state
+            state_prime = max(
+                self.mdp.get_transition_matrices()[action].get_matrix()[state].items(),
+                key=lambda state_prob_pair: state_prob_pair[1]
+            )[0]
+            
+            # Update state and reward
+            self.reward = state_prime.get_reward()
+            self.state = state_prime
 
-    def step(self):
-        raise (NotImplementedError)
-        # action = select action(state)
-        self.trajectory.states.append(self.state)
-        self.trajectory.actions.append(action)
-        self.trajectory.actions.append(self.reward)
+            depth += 1   
+            
+            
 
-        # self.state, self.reward = action_result
+    
