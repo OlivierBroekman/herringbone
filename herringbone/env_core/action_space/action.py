ba1526c8e841a82bb2cb18bc6a29ce3fab5246a8
Olivier Broekman <olivier.broekman@ru.nl>
2025-03-17 11:09:46 +0100

    All changes are for consistency, along with removal of some setters

--- herringbone/env_core/action_space/action.py
+++ herringbone/env_core/action_space/action.py
@@ -1,9 +1,9 @@
 class Action:
     """This class represents the actions for a reinforcement learning problem."""
     def __init__(
-            self, 
+            self,
             config: dict
-            ):
+    ):
         
         self.__id = config["id"]
         self._type = config["type"]
@@ -12,82 +12,41 @@ class Action:
         assert sum(config["probabilities"]) == 1
         assert len(config["probabilities"]) == len(config["directions"])
         self._cost = config["cost"]
-        self._value = 0
         self._character = config["char"]
 
     def __str__(self):
-        return f"{self.__id}"
+        return f"{self._character}"
     
     def __repr__(self):
         return f"{self._character}"
-    # Setters and getters
-    def set_type(
-            self, 
-            new_type: str
-            ): 
-        
-        self._type = new_type
-    
+   
+    # Getters
     def get_type(
             self
-            ) -> str:
+    ) -> str:
         
         return self._type
-    
-    def set_directions(
-            self, 
-            new_directions: list[list[int]]
-            ):
-        
-        self._directions = new_directions
 
     def get_directions(
             self
-            ) -> list[list[int]]:
+    ) -> list[list[int]]:
         
         return self._directions
-    
-    def set_probabilities(
-            self, 
-            new_probs: list[float]
-            ):
-        
-        self._probabilities = new_probs
 
     def get_probabilities(
             self
-            ) -> list[float]:
+    ) -> list[float]:
         
         return self._probabilities
     
-    def set_cost(
-            self, 
-            new_cost: int
-            ):
-        
-        self._cost = new_cost
-    
     def get_cost(
             self
-            ) -> int:
+    ) -> int:
         
         return self._cost
-    
-    def set_value(
-            self, 
-            new_val: float
-            ):
-        
-        self._value = new_val
-    
-    def get_value(
-            self
-            ) -> float:
-        
-        return self._value
 
     def get_character(
             self
-            ) -> float:
+    ) -> str:
         
         return self._character
