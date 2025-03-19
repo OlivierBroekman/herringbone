033fdb10d80130be56d2d11408a33b7caa8b62b3
jiworo <pangilon@protonmail.com>
2025-03-19 17:01:23 +0100

    Make State hashable; key=coordinates

--- herringbone/env_core/state_space/state.py
+++ herringbone/env_core/state_space/state.py
@@ -1,14 +1,13 @@
 class State:
     def __init__(
-            self,
-            is_terminal: bool,
-            location: list[int],
-            reward: float,
-            is_visitable: bool,
-            character: str,
-            color: str,
+        self,
+        is_terminal: bool,
+        location: list[int],
+        reward: float,
+        is_visitable: bool,
+        character: str,
+        color: str,
     ):
-        
         self._is_terminal = is_terminal
         self._location = location
         self._reward = reward
@@ -16,77 +15,43 @@ class State:
         self._character = character
         self._color = color
 
-    def __str__(
-            self
-    ):
+    def __str__(self):
         return self._character
-    
-    def __repr__(
-            self
-    ):
+
+    def __repr__(self):
         return str(self._location)
-    
-    def __eq__(
-            self,
-            other
-    ):
-        assert type(self) == type(other)
+
+    def __eq__(self, other):
+        assert isinstance(other, State)
         return self.get_location() == other.get_location()
-    
-    # Setters and getters    
-    def get_is_terminal(
-            self
-            ) -> bool: 
-        
+
+    def __hash__(self):
+        return hash(tuple(self._location))
+
+    # Setters and getters
+    def get_is_terminal(self) -> bool:
         return self._is_terminal
 
-    def set_location(
-            self, 
-            new_loc: list[int]
-    ):
-        
+    def set_location(self, new_loc: list[int]):
         self._location = new_loc
 
-    def get_location(
-            self
-    ) -> list[int]:
-        
+    def get_location(self) -> list[int]:
         return self._location
-    
-    def get_reward(
-            self
-    ) -> float:
-        
+
+    def get_reward(self) -> float:
         return self._reward
 
-    def get_is_visitable(
-            self
-    ) -> bool:
-        
+    def get_is_visitable(self) -> bool:
         return self._is_visitable
-    
-    def set_character(
-            self, 
-            new_char: str
-    ):
-        
+
+    def set_character(self, new_char: str):
         self._character = new_char
-    
-    def get_character(
-            self
-    ) -> str:
-        
+
+    def get_character(self) -> str:
         return self._character
 
-    def set_color(
-            self, 
-            new_color: str
-    ):
-        
+    def set_color(self, new_color: str):
         self._color = new_color
-    
-    def get_color(
-            self
-    ) -> str:
-        
+
+    def get_color(self) -> str:
         return self._color
