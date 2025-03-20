963adecb46af2d5738da181e3ed33dac1ca11b67
jiworo <pangilon@protonmail.com>
2025-03-21 00:49:35 +0100

    Resolve issue #54

--- herringbone/env_core/utils/map_loader.py
+++ herringbone/env_core/utils/map_loader.py
@@ -6,9 +6,7 @@ from herringbone.env_core.utils.config_loader import load_config
 from herringbone.env_core.state_space.state import State
 
 
-def read_map(
-    path_map: Path
-) -> list[list[int]]:
+def read_map(path_map: Path) -> list[list[int]]:
     try:
         with open(path_map, 'r', encoding='utf-8') as map_:
             return [[int(cell) for cell in row] for row in reader(map_)]
@@ -18,10 +16,7 @@ def read_map(
         raise ValueError(f"Cannot decode CSV '{path_map}'")
 
 
-def init_state(
-    state_vals: dict[str, Any],
-    coordinates: list[int]
-) -> State:
+def init_state(state_vals: dict[str, Any], coordinates: list[int]) -> State:
     try:
         return State(
             is_terminal=state_vals["is_terminal"],
@@ -35,15 +30,19 @@ def init_state(
         raise KeyError(f"State has no attribute: {e}") from e
 
 
-def load_map(
-    path_config: Path,
-    path_map: Path
-) -> list[list[State]]:
+def load_map(path_config: Path, path_map: Path) -> list[list[State]]:
     config = load_config(Path(path_config))
     map_ = read_map(Path(path_map))
 
+    def get_state(state_id: int, coordinates: list[int]):
+        try:
+            return init_state(config[str(state_id)], coordinates)
+        except KeyError:
+            raise KeyError(
+                f"Unknown state ID '{state_id}' found at {coordinates} in {path_map}."
+            )
+
     return [
-        # TODO no default specified for config.get()
-        [init_state(config.get(str(state_id)), [x, y]) for y, state_id in enumerate(row)]
+        [get_state(state_id, [x, y]) for y, state_id in enumerate(row)]
         for x, row in enumerate(map_)
     ]
