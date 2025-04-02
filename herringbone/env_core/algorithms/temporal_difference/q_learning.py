710e442326e0f2cce29f37b52ebbfaf86209e886
jiworo <pangilon@protonmail.com>
2025-04-02 22:00:00 +0200

    Add comments

--- herringbone/env_core/algorithms/temporal_difference/q_learning.py
+++ herringbone/env_core/algorithms/temporal_difference/q_learning.py
@@ -15,6 +15,7 @@ class QLearning(TDControl):
 
         Pseudocode adapted from: Sutton, R. S., & Barto, A. G. (2018). Reinforcement learning: An introduction (2nd ed.). The MIT Press.
         """
+        
         action_max = max(self.q_values[state_prime], key=self.q_values[state_prime].get)
 
         self.q_values[state][action] += self.alpha * (
@@ -30,15 +31,28 @@ class QLearning(TDControl):
 
         Pseudocode adapted from: Sutton, R. S., & Barto, A. G. (2018). Reinforcement learning: An introduction (2nd ed.). The MIT Press.
         """
+        
+        # Loop for each episode
         for _ in range(self.num_episodes):
+
+            # Initialize S
             state = self.mdp.get_start_state()
 
+            # Loop for each step of episode until S is terminal
             while not state.get_is_terminal():
+
+                # Choose A from S using policy derived from Q (e.g., epsilon-greedy)
                 action = self.policy.get_next_action(state, self.q_values)
+
+                # Take action A, observe S', R
                 state_prime = self.mdp.get_next_state(state, action)
                 self.reward_last = state_prime.get_reward()
                 self.rewards.append(self.reward_last)  # For analysis
+
+                # Q(S, A) ← A(S, A) + α[R + γ max_a Q(S', a) - Q(S, A)]
                 self.update_q_values(state, action, state_prime)
+
+                # S ← S'
                 state = state_prime
 
             self.decay_epsilon()
